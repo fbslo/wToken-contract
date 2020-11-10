da6ac1c8ee93cdcdcd8248629716cb82025a6679
fbslo <admin@fbslo.net>
2020-11-10 11:27:41 +0100

    fix network id

--- truffle-config.demo.js
+++ truffle-config.demo.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const PrivateKeyProvider = require("truffle-privatekey-provider");
 require("ts-node/register");
+var Web3 = require('web3');
 require('dotenv').config()
 
 var privateKey = process.env.PRIVATE_KEY;
 var ethereumEndpoint = process.env.ETHEREUM_ENDPOINT
+var web3 = new Web3(process.env.ETHEREUM_ENDPOINT);
 
-var id = 1
-if (ethereumEndpoint.includes("ropsten")) id = 3
+var id = web3.eth.net.getId() || 1
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "build"),
