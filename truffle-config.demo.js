5668b1c050d7d184992703caacd4f997db2d78d0
fbslo <admin@fbslo.net>
2020-11-10 11:32:00 +0100

    fix config, remove id

--- truffle-config.demo.js
+++ truffle-config.demo.js
@@ -1,21 +1,17 @@
 const path = require("path");
 const PrivateKeyProvider = require("truffle-privatekey-provider");
 require("ts-node/register");
-var Web3 = require('web3');
 require('dotenv').config()
 
 var privateKey = process.env.PRIVATE_KEY;
 var ethereumEndpoint = process.env.ETHEREUM_ENDPOINT
-var web3 = new Web3(process.env.ETHEREUM_ENDPOINT);
 
-var id = web3.eth.net.getId() || 1
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "build"),
   networks: {
     mainnet: {
-     provider: () => new PrivateKeyProvider(privateKey, ethereumEndpoint),
-     network_id: id
+     provider: () => new PrivateKeyProvider(privateKey, ethereumEndpoint)
    }
   },
   mocha: {
