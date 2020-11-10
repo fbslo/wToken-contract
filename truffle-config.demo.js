339ff7ecb3c51a5915d19342811c6247538688dd
fbslo <admin@fbslo.net>
2020-11-10 11:43:21 +0100

    fix config id

--- truffle-config.demo.js
+++ truffle-config.demo.js
@@ -6,15 +6,12 @@ require('dotenv').config()
 var privateKey = process.env.PRIVATE_KEY;
 var ethereumEndpoint = process.env.ETHEREUM_ENDPOINT
 
-var id = 1
-if (ethereumEndpoint.includes('ropsten')) id = 3
-
 module.exports = {
   contracts_build_directory: path.join(__dirname, "build"),
   networks: {
     mainnet: {
      provider: () => new PrivateKeyProvider(privateKey, ethereumEndpoint),
-     network_id: id
+     network_id: "*"
    }
   },
   mocha: {
